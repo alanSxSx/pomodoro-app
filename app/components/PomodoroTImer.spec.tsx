898968f7041754d99871d328ab617eb038cd9893
Alan Barbosa <alanzinhob@hotmail.com>
2024-03-22 16:01:19 -0300

    22_03

--- app/components/PomodoroTImer.spec.tsx
+++ app/components/PomodoroTImer.spec.tsx
@@ -1,6 +1,23 @@
 import { render } from '@testing-library/react'
 import PomodoroTimer from './PomodoroTimer';
 
+jest.mock('/sounds/bell-start.mp3', () => {
+    return function() {
+      return { play: jest.fn() };
+    };
+  });
+  jest.mock('/sounds/bell-finish.mp3', () => {
+    return function() {
+      return { play: jest.fn() };
+    };
+  });
+  Object.defineProperty(document.body, 'classList', {
+    value: {
+      add: jest.fn(),
+      remove: jest.fn()
+    }
+  });
+
 
 describe('Pomodoro Component', () => {
 
@@ -8,7 +25,12 @@ describe('Pomodoro Component', () => {
 
         const { getByTestId } = render(<PomodoroTimer pomodoroTime={1500} shortRestTime={300} longRestTime={900} cycles={4}/>);
         expect(getByTestId('pomodoro-timer')).toBeInTheDocument()
-       
+
     })
+
+    it('deve iniciar com o tempo padrão do pomodoro', () => {
+        const { getByTestId } = render(<PomodoroTimer pomodoroTime={1500} shortRestTime={300} longRestTime={900} cycles={4} />);
+        expect(getByTestId('pomodoro-timer')).toHaveTextContent('25:00');
+      });
 
 })
